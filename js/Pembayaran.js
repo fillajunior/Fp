271871a3524f6340bb696705d92849ea5117defd
Elyz1309 <darman.3115@students.amikom.ac.id>
2021-06-29 09:37:12 +0700

    Darman V.0.4

---
+++ js/Pembayaran.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    var a;
+    var b;
+    $('.ItemPembayaran').click(function(){
+        cek = $('input[name = "ItemP"]:checked').val();
+        item = $('input[name = "ItemP"]:checked').attr('id');
+        a = cek;
+        $('#Harga').val(cek);
+        $('#ItemPembayaran').val(item);
+    });
+
+    $('.MetodePembayaran input:checkbox').click(function() {
+        $('.MetodePembayaran input:checkbox').not(this).prop('checked', false);
+        metode = $('input[name = "MetodeP"]:checked').attr('id');
+        b = metode;
+        $('#MetodePembayaran').val(metode);
+    });
+
+    $('button[type="submit"]', '.Konfirm').click(function(){
+        if($('#Bukti').val() == ''){
+            alert("Bukti Belum diupload");
+        }else if(a == null){
+            alert("Item Pembayaran Belum Dipilih");
+        }else if(b == null){
+            alert("Metode Pembayaran Belum Dipilih");
+        }else{
+            alert("Pembayaran Berhasil");
+            window.location.href = "Pembayaran.html";
+        }
+    });
+
+    $('.logo').click(function(){
+        window.location.href = "Profile.html";
+    });
+});
