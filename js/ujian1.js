3481dc3d32d7bf691739b45dee4cd42474212a96
fillajunior <pantailovina@gmail.com>
2021-07-21 15:52:09 +0800

    update 21/07/21

--- js/ujian1.js
+++ js/ujian1.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $('#subtn').click(function () {
-        if($('#Jawaban').val==1) {
+        if($('#Jawaban').val() != '') {
+            var file = $('#Jawaban').val()
+            var name = $('#name').val()
             alert ('Jawaban Berhasil Di Upload');
-            $('#jawaban1').append('<input type="radio" id="mapel1" class="input-form-chackbox"> <label for ="mapel1" >' + label1 + ' </label>')
+            $('#jawaban1').append('<div class="card" style="width: 18rem;"> <div class = "card-body" ><h5 class = "card-title" > ' + name + ' </h5> <h6 class = "card-subtitle mb-2 text-muted" > ' + file.substring(12, 100) + ' </h6></div> </div>')
             $('#Jawaban').val('')
         }
         else {
